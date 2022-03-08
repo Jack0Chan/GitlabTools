aa2f6a7ea7047fbb7c7f99f8382f63075ac76b96
Jack0Chan <48609550+Jack0Chan@users.noreply.github.com>
2022-03-09 00:30:06 +0800

    Finish

---
+++ GitLabTools.py
@@ -0,0 +1,81 @@
+from playwright.sync_api import sync_playwright
+from typing import List
+import pandas as pd
+import time
+
+
+class GitLabTools:
+    def __init__(self, gitlab_url: str):
+        self.url = gitlab_url
+
+        self.__playwright = sync_playwright().start()
+        self.__browser = self.__playwright.chromium.launch(headless=False)
+        self.__context = self.__browser.new_context()
+        self.__page = self.__context.new_page()
+
+    def __del__(self):
+        self.__context.close()
+        self.__browser.close()
+        self.__playwright.stop()
+
+    def sign_in(self, account: str, password: str):
+        sign_in_url = self.url + "/users/sign_in"
+        sign_in_url = sign_in_url.replace('//', '/')
+
+        self.__page.goto(sign_in_url)
+        self.__page.locator("input[name=\"user\\[login\\]\"]").fill(account)
+        self.__page.locator("input[name=\"user\\[password\\]\"]").fill(password)
+        self.__page.locator("text=Sign in").click()
+        time.sleep(0.2)
+        return None
+
+    def create_new_account(self, name: str, username: str, mail: str):
+        add_new_url = self.url + '/admin/users/new'
+        add_new_url = add_new_url.replace('//', '/')
+
+        self.__page.goto(add_new_url)
+        self.__page.locator("input[name=\"user\\[name\\]\"]").fill(name)
+        self.__page.locator("input[name=\"user\\[username\\]\"]").fill(username)
+        self.__page.locator("input[name=\"user\\[email\\]\"]").fill(mail)
+        time.sleep(0.5)
+        self.__page.locator("text=Create user").click()
+        # 最少等3秒再发送邮件，防止被ban
+        time.sleep(3)
+        return None
+
+    def create_new_group(self, group_name: str):
+        create_group_url = self.url + '/groups/new'
+        create_group_url = create_group_url.replace('//', '/')
+
+        self.__page.goto(create_group_url)
+        self.__page.locator("h3:has-text(\"Create group\")").click()
+        # 组名
+        self.__page.locator('input#group_name').fill(group_name)
+        # 组等路径
+        self.__page.locator('input#group_path').fill(group_name)
+        with self.__page.expect_navigation():
+            self.__page.locator("input:has-text(\"Create group\")").click()
+        time.sleep(1)
+        return None
+
+    def invite_group_members(self, group_name: str, members: List[str], role='Maintainer'):
+        """
+        members可以是邮箱，可以是用户名
+        """
+        group_url = self.url + f'/groups/{group_name}/-/group_members'
+        group_url = group_url.replace('//', '/')
+
+        # 页面一直在刷新，会超时。我们等待commit结束就可以了，没必要等刷新完
+        self.__page.goto(group_url, wait_until='commit')
+
+        # 添加成员
+        for member in members:
+            self.__page.locator("#s2id_autogen1").fill(member)
+            time.sleep(0.5)
+            self.__page.locator("#s2id_autogen1").press("Enter")
+        # 设置权限
+        level_dict = {'Guest': '10', 'Reporter': '20', 'Developer': '30', 'Maintainer': '40', 'Owner': '50'}
+        self.__page.locator('select#access_level').select_option(level_dict[role])
+        # 点击邀请
+        self.__page.locator("[data-testid=\"invite-users-form\"] >> text=Invite").click()
+        return None
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # GitlabTools
 Help to create new accounts, new groups and invite members to groups. Implemented with playwright.
+
+# Environment
+
+`pip install playwright`
+
+- [playwright 入门教程](https://www.yuque.com/docs/share/4f709c9d-b649-4450-b791-4827c1e41e02?#)
+
+# Usage
+
+```python
+from GitLabTools import GitLabTools
+
+students_info = pd.read_csv('students_profile.csv')
+students_info = students_info.values
+"""print(students_info)
+[[666666 '王王王' 'xx学院' 'wang@xxx.xx' '学生']
+[777777 '李李李' 'xx学院' 'li@xx.xx' '学生']]
+"""
+
+gitlab = GitLabTools(gitlab_url='http://xxxxx/')
+gitlab.sign_in(account='xxx', password='xxx')
+
+# create new accounts
+for student_info in students_info:
+		mail = student_info[3]
+		username = mail[:mail.index('@')]
+  	gitlab.create_new_account(name=username, username=username, mail=mail)
+
+# create new groups
+for i in range(11):
+		team_name = f'team{i+1}'
+		# print(team_name)
+		gitlab.create_new_group(team_name)
+
+# invite students to group
+gitlab.invite_group_members(group_name='test', members=['wang', 'chen'])
+```
+
